394305323d9a1890538b18fa398afa594438f524
Sean Mackert <smackert@users.noreply.github.com>
2021-10-27 19:39:29 +0400

    bufio implementation and speed fix

--- main.go
+++ main.go
@@ -10,39 +10,26 @@ import (
 )
 
 //var inputFile string = ""
-var bufferSize int64 = 512
-
-//var mapFile string = ""
+var chunkSize int64 = 512
+var readBufferSize int
+var writeBufferSize int
 
 func main() {
 
 	inputFile := flag.String("f", "", "Path to the file you wish to analyze.")
-	bufferSize = *flag.Int64("s", 512, "Define sector size in bytes.")
+	chunkSize = *flag.Int64("s", 512, "Define sector size in bytes.")
+	readBufferSize = *flag.Int("rb", 40, "Read buffer size in MB")
+	writeBufferSize = *flag.Int("wb", 40, "Write buffer size in MB")
 	flag.Parse()
 	if *inputFile == "" {
 		fmt.Println("A file name is required. Exiting..")
 		os.Exit(0)
 	}
 
-	//const binaryExe = "test_data/binary.exe"
-	//const text = "test_data/sample1.txt"
-	//const volImg = "test_data/test_volume.img" // 599mb (628,097,024 bytes) contains 100mb and 50mb encrypted volumes
-	//const movie = "test_data/movie.mkv"        // 4.37 GB (4,695,146,496 bytes)
-	//const encVol = "test_data/encrypted_volume" // 600mb (629,145,600 bytes)
-	//const loremipsum = "test_data/loremipsum.txt"
-	//const bufferSize int64 = 837463 // 1mb (1000000)
-
 	startTime := time.Now()
 	ReadFile(*inputFile)
 	fmt.Printf("Completed in %s", time.Since((startTime).Truncate(time.Second)))
 
-	//ReadFile(volImg, bufferSize)
-	//eadFile(movie, bufferSize)
-	//ReadFile(encVol, bufferSize)
-	//ReadFile(binaryExe, bufferSize)
-	//ReadFile(loremipsum, bufferSize)
-
-	//fmt.Println("Done")
 }
 
 func errCheck(err error) {
@@ -57,33 +44,32 @@ func ReadFile(filePath string) {
 	errCheck(err)
 	defer file.Close()
 
-	f, err := file.Stat()
+	filestat, err := file.Stat()
 	errCheck(err)
 
-	fmt.Println("File:", f.Name(), "\nSize:", f.Size(), "\nChunks:", (f.Size() / bufferSize))
+	totalChunks := filestat.Size() / chunkSize
+	fmt.Println("File:", filestat.Name(), "\nSize:", filestat.Size(), "\nChunks:", totalChunks)
 
 	var offset int64
-	//writeChan := make(chan map[int64]int, 5000)
 	chunk := 0
 	mapFile := makeMapFile(filePath)
-	bw := bufio.NewWriterSize(mapFile, 1048576)
-	/*
-		    for offset
-			inputdata = read up to 1mb
-			for len(inputdata) / buffersize
-				go ent(inputdata, bufoffset)
-				bufoffset += buffesize
-			offset += len(inputdata)
-	*/
-
-	for offset = 0; (offset + bufferSize) < f.Size(); offset += bufferSize { // this cuts the tail off. fix that l8r
-		calcEntropy(*file, offset, bw)
+	br := bufio.NewReaderSize(file, readBufferSize*1048576)     //  1048576 = 1mb
+	bw := bufio.NewWriterSize(mapFile, writeBufferSize*1048576) //  1048576 = 1mb
+	progress := 0
+	for offset = 0; (offset + chunkSize) < filestat.Size(); offset += chunkSize { // this might cut the tail off. check that l8r
+		ent := calcEntropy(br, chunkSize)
+		writeMap(offset, ent, bw)
 		chunk += 1
-		fmt.Printf("\r%f%% Complete..", (float32(float32(chunk)/(float32(f.Size())/float32(512))) * float32(100)))
+		curProgress := int((float32(chunk) / float32(totalChunks)) * float32(100))
+		if curProgress >= progress+1 {
+			fmt.Printf("\r%v%% complete...", curProgress)
+			progress = curProgress
+		}
 	}
-	//bw.Flush()
+
+	bw.Flush()
 	mapFile.Close()
-	fmt.Println("\nFinished!")
+	fmt.Println("Finished!")
 }
 
 func makeMapFile(fp string) *os.File {
@@ -95,25 +81,16 @@ func makeMapFile(fp string) *os.File {
 
 }
 
-func calcEntropy(file os.File, offset int64, bw *bufio.Writer) {
-	buf := make([]byte, bufferSize)
-	_, err := file.ReadAt(buf, offset)
+func calcEntropy(br *bufio.Reader, chunkSize int64) int {
+	buf := make([]byte, chunkSize)
+	_, err := br.Read(buf)
 	errCheck(err)
-	se := make(map[int64]int)
-	se[offset] = Shannon(buf[:])
-	writeMapBuf(se, bw)
+	return Shannon(buf[:])
 }
 
-func writeMapBuf(se map[int64]int, bw *bufio.Writer) {
-	/*
-		    var writeBuffer = make(map[int64]int)
-			for k, v := range data {
-				writeBuffer[k] = v
-			}
-	*/
-	for k, v := range se {
-		_, err := bw.WriteString(strconv.FormatInt(k, 10) + "," + strconv.Itoa(v) + "\n")
-		errCheck(err)
-	}
+func writeMap(offset int64, ent int, bw *bufio.Writer) {
+
+	_, err := bw.WriteString(strconv.FormatInt(offset, 10) + "," + strconv.Itoa(ent) + "\n")
+	errCheck(err)
 
 }
